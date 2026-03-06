34f81bc55664a92d8bde0ad922f1c1bff3a5aad7
Leidy Bautista <analistaintegradordgics2@gmail.com>
2026-03-06 14:10:15 -0500

    ajuste en index components

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
-import LInput from './components/input.vue'
-import LInputDecimal from './components/inputdecimal.vue'
-import LMoney from './components/money.vue'
-import LNewModal from './components/newmodal.vue'
-import LSelect from './components/select.vue'
-import LSelectApi from './components/selectApi.vue'
-import LSelectRest from './components/selectRest.vue'
-import LTable from './components/table.vue'
-import LTipoDocumento from './components/tipoDocumento.vue'
-import LInmueblesAsociados from './components/inmueblesasociados.vue'
-
 import http from './mixins/http'
 import mensajes from './mixins/mensajes'
 
-const components = [
-  LInput,
-  LInputDecimal,
-  LMoney,
-  LNewModal,
-  LSelect,
-  LSelectApi,
-  LSelectRest,
-  LTable,
-  LTipoDocumento,
-  LInmueblesAsociados
-]
+const requireComponent = require.context(
+  './components/l', 
+  false,
+  /\.vue$/
+)
 
 export default {
   install(Vue) {
 
-    components.forEach(component => {
+    requireComponent.keys().forEach(fileName => {
+
+      const componentConfig = requireComponent(fileName)
+
+      const component = componentConfig.default || componentConfig
+
       Vue.component(component.name, component)
+
     })
 
     Vue.mixin(http)
